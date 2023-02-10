190717e7ab9debecc31c7b5658bbb26cd5245d87
happyjino <wlsgh1128@naver.com>
2023-02-11 00:23:28 +0900

    del test_mbti

--- test_mbti/src/components/MyInput.js
+++
@@ -1,9 +0,0 @@
-const MyInput = ({ name, type }) => {
-  if (type === "radio") {
-    return <input className="MyRadio" type="radio" name={name} />;
-  }
-
-  return <input className="MyInput" type={type} placeholder={name} />;
-};
-
-export default MyInput;
